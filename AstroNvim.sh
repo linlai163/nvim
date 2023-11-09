289370bb9eee01c3a5c1c30e6ee80020e2ae8a87
linlai163 <linlai163@outlook.com>
2023-11-09 20:20:48 +0800

    Update config

--- AstroNvim.sh
+++
@@ -1,8 +0,0 @@
-rm -rf ~/.config/nvim ~/.config/nvim
-rm -rf ~/.local/share/nvim ~/.local/share/nvim.bak
-rm -rf ~/.local/state/nvim ~/.local/state/nvim.bak
-rm -rf ~/.cache/nvim ~/.cache/nvim.bak
-echo "delete successful"
-git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
-nvim
-
